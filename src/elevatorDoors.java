f68d5690c55e6e724704ca826fe201b52d25c892
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2019-01-28 12:54:57 -0500

    -created the elevator doors

---
+++ src/elevatorDoors.java
@@ -0,0 +1,19 @@
+
+public class elevatorDoors {
+	
+	private boolean doorState;
+
+	public elevatorDoors() {
+		this.setDoorState(false);
+	}
+	
+	public boolean getDoorState() {
+		return doorState;
+	}
+	
+	public void setDoorState(boolean doorState) {
+		this.doorState = doorState;
+	}
+
+
+}
