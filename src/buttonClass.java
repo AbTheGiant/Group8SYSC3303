372cc2015149f950382e8611abc6ec9c3e9c1cbc
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2019-01-28 11:51:14 -0500

    -created the class for buttons

---
+++ src/buttonClass.java
@@ -0,0 +1,15 @@
+
+public class buttonClass {
+	private boolean pushed;
+	public buttonClass() {
+		this.setButton(false);
+	}
+	
+	public void setButton(boolean temp) {
+		this.pushed=temp;
+	}
+
+	public boolean getButton() {
+		return pushed;
+	}
+}
