bf98c13ef6c86a68d7cbb7da9245bdf89ad17c19
Abiola Olajide <abiolaolajide@dhcp-87-248.cu-internet.carleton.ca>
2019-01-29 12:49:39 -0500

    -skelenton of floor subsystem

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -63,66 +63,7 @@ public class FloorSubsystem {
 
 	public void send()
 	{
-		byte sendData[] = new byte[4];
-		   
-	     
-	     
-	    
-	     
-	     
-	    try {
-			if (receivePacket.getData()[0]==0 && receivePacket.getData()[1]==1) {
-				sendData[0]=0;
-				sendData[1]=3;
-				sendData[2]=0;
-				sendData[3]=1;
-				sendPacket= new DatagramPacket(sendData,sendData.length,
-			    		  receivePacket.getAddress(), receivePacket.getPort());
-
-				
-			}else if (receivePacket.getData()[0]==0 && receivePacket.getData()[1]==2) {
-				sendData[0]=0;
-				sendData[1]=4;
-				sendData[2]=0;
-				sendData[3]=0;
-				sendPacket= new DatagramPacket(sendData,sendData.length,
-			    		  receivePacket.getAddress(), receivePacket.getPort());
-			}else {
-				System.err.println("Error");
-				throw new Exception("Invalid packet");
-			}
-		} catch (Exception e) {
-			
-			 e.printStackTrace();
-	         System.exit(1);
-		}
-	     
-	     System.out.println(""
-	     		+ " "
-	     		+ " ");
-	     
-	    //print the content of the packet
-	     System.out.println( "Server: Sending packet to intermediate:");
-	      System.out.println("To host: " + sendPacket.getAddress());
-	      System.out.println("Destination host port: " + sendPacket.getPort());
-	     int  len = sendPacket.getLength();
-	      System.out.println("Length: " + len);
-	      System.out.print("Containing: ");
-	     
-	      
-	      for (int i = 0; i < sendPacket.getLength(); i++) {
-			System.out.print(sendPacket.getData()[i]);
-	      }
-	      System.out.println("");
-		     //sending the new packet back to the intermediate
-	      try {
-	         sendReceiveSocket.send(sendPacket);
-	      } catch (IOException e) {
-	         e.printStackTrace();
-	         System.exit(1);
-	      }
-
-	      System.out.println("Server: packet sent");	
+		
 		
 	}
 	
@@ -138,13 +79,8 @@ public class FloorSubsystem {
 	
 	public static void main( String args[] )
 	{
-	   FloorSubsystem c = new FloorSubsystem();
-	  // File simulate = new File (getfile ();
+	 
 	  
-	  while(c!=null) {
-	   c.send();
-	   c.receive();
-	  }
 	}
 	
 }
